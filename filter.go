74a8ea273a41e24d070743a8b90acf6bbcdb0c20
Chen Tao <welkinv@gmail.com>
2015-09-23 15:05:15 +0800

    BylogFilter: 每小时将BylogMetric消息传递出来，并重新开始计数；发送间隔可配置（用于测试）

--- filter.go
+++ filter.go
@@ -3,14 +3,23 @@ package csv
 import (
 	"fmt"
 	. "github.com/mozilla-services/heka/pipeline"
+	"os"
+	"strings"
 	"sync"
+	"time"
 )
 
 type BylogFilter struct {
-	counter Counter
+	config   *BylogFilterConfig
+	counter  Counter
+	hostname string
+	pid      int
+	fr       FilterRunner
+	h        PluginHelper
 }
 
 type BylogFilterConfig struct {
+	MetricInterval int `toml:"metric_interval"`
 }
 
 type MetricType int
@@ -43,13 +52,43 @@ func (this *BylogFilter) ConfigStruct() interface{} {
 }
 
 func (this *BylogFilter) Init(config interface{}) error {
+	this.config = config.(*BylogFilterConfig)
+
 	this.counter.m = make(map[Key]Value)
+	this.hostname, _ = os.Hostname()
+	this.pid = os.Getpid()
+
+	go func() {
+		var timer *time.Ticker
+		for {
+			now := time.Now()
+			truncated := now.Add(time.Duration(1) * time.Hour).Truncate(time.Hour)
+
+			var sleep time.Duration
+			if this.config.MetricInterval == 0 {
+				sleep = truncated.Sub(now)
+			} else {
+				sleep = time.Duration(this.config.MetricInterval) * time.Second
+			}
+			timer = time.NewTicker(sleep)
+			LogInfo.Printf("Now is %s, going to sleep %s\n", now.String(), sleep.String())
+
+			select {
+			case <-timer.C:
+				this.cleanMetricCounter(false)
+			}
+			timer.Stop()
+		}
+	}()
+
 	return nil
 }
 
 func (this *BylogFilter) Run(fr FilterRunner, h PluginHelper) (err error) {
-	var pack *PipelinePack
+	this.fr = fr
+	this.h = h
 
+	var pack *PipelinePack
 	inChan := fr.InChan()
 	for pack = range inChan {
 		fields := pack.Message.GetFields()
@@ -79,6 +118,7 @@ func (this *BylogFilter) Run(fr FilterRunner, h PluginHelper) (err error) {
 		pack.Recycle()
 	}
 
+	this.cleanMetricCounter(true)
 	return
 }
 
@@ -103,4 +143,45 @@ func (f *BylogFilter) doMetric(bpid, api string, t MetricType) {
 		f.counter.m[Key{bpid, api}] = new_value
 	}
 	f.counter.Unlock()
+}
+
+func (f *BylogFilter) cleanMetricCounter(exiting bool) {
+	num := 0
+	f.counter.Lock()
+	for k, v := range f.counter.m {
+		d, _ := time.ParseDuration("-1h")
+		t := time.Now().Add(d)
+		if exiting == true {
+			t = time.Now()
+		}
+		var arr []string
+		arr = append(arr, t.Truncate(time.Hour).Format("2006-01-02 15:04:05.999999999"))
+		arr = append(arr, k.bpid)
+		arr = append(arr, k.api)
+		arr = append(arr, fmt.Sprintf("%s:%d", f.hostname, f.pid))
+		for _, count := range v.counts {
+			arr = append(arr, fmt.Sprintf("%d", count))
+		}
+		point := strings.Join(arr, ",")
+		num++
+		f.deliverMetric(point + "\n")
+	}
+	f.counter.m = make(map[Key]Value)
+	f.counter.Unlock()
+
+	LogInfo.Println("This hour: num of points in counter:", num)
+}
+
+func (f *BylogFilter) deliverMetric(point string) {
+	const msgType = "BylogMetric"
+
+	pack := f.h.PipelinePack(0)
+	if pack == nil {
+		LogError.Println("exceeded MaxMsgLoops =", f.h.PipelineConfig().Globals.MaxMsgLoops)
+		return
+	}
+	pack.Message.SetLogger(f.fr.Name())
+	pack.Message.SetType(msgType)
+	pack.Message.SetPayload(point)
+	f.fr.Inject(pack)
 }
